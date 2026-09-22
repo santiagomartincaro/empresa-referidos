7bec81253e422ebc0d7695699d2ec34313fd3679
Santi <santi@example.com>
2026-09-23 00:31:41 +0200

    feat: add Google site verification and configure @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://compramaestra.santiagomartincaro.workers.dev',
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
+    sitemap(),
   ],
 });
